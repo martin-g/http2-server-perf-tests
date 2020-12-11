558e61b750c7b5b3e80041c3eb61ba6c31e5ac57
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-12-11 11:25:52 +0200

    Update the app to Jakarta EE 9

--- java/tomcat/src/main/java/info/mgsolutions/tomcat/AsyncPlainTextServlet.java
+++ java/tomcat/src/main/java/info/mgsolutions/tomcat/AsyncPlainTextServlet.java
@@ -1,11 +1,11 @@
 package info.mgsolutions.tomcat;
 
-import javax.servlet.AsyncContext;
-import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
